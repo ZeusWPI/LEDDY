08a28764dc4b1b84a41096cbc4f393509c5326b9
klagoeth <klaas.goethals@ugent.be>
2023-01-18 02:13:26 +0100

    Working server with commands

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 ## LED DisplaY
 
+#### Control LEDDY
+
+Currently only through HTTP POST requests.
+```
+Usage: HTTP Post with the following plaintext body:
+
+COMMAND [command opts ...]
+
+Commands:
+    ScrollingText <text>
+    ClearDisplay
+```
+Example:
+```bash
+$ echo "ScrollingText Welkom in de kelder!" | http POST 10.1.0.181
+```
+
 #### Hardware
 
-- Arduino Uno
+- ESP8266 aka Servy: handles outside communication
+- Arduino Uno: controlling the LEDS
 - 12x MAX7219 Led display (8x8 leds)
 - Wired like this _(source: the internet)_:
 <img src="https://user-images.githubusercontent.com/47608311/211937380-e48b0876-e36b-4873-9c20-ebee0466bb67.png" height="500px" />
 
 
-#### Notes on implementation
+#### Notes on LED control implementation
 
 - The LedControl library can only handle 8 displays chained. We have 12 displays. 4 chains of each 3 displays. Displays are chained on the same bus, more displays on the same bus causes slow text scrolling. This is the reason they are split into 4 different busses with each only 3 displays.
 - To set or unset leds on the display:
@@ -21,10 +39,16 @@
 
 #### Servy
 
-- ESP8266
-- [MicroPython installed](http://micropython.org/download/#esp8266)
-- Use the WebREPL in `servy/webrepl/webrepl.html` (submodule) to update or execute code on Servy.
-- IP: 10.1.0.181, WebREPL on ws://10.1.0.181:8266
+- IP: 10.1.0.181
+- Running [MicroPython](https://docs.micropython.org/en/latest/esp8266/tutorial/intro.html#getting-the-firmware)
+- Write files to microcontroller. `boot.py` and `main.py` are executed on boot.
+    1. Connected through serial with `ampy`:
+    ```bash
+    $ ampy -p /dev/ttyUSB0 -b 115200 put main.py main.py
+    # Or directly run file while developing
+    $ ampy -p /dev/ttyUSB0 -b 115200 run main.py
+    ```
+    2. Use the WebREPL in `servy/webrepl/webrepl.html` (submodule) to update or execute code on Servy. Connect with `ws://10.1.0.181:8266`
 
 #### Development
 
@@ -33,4 +57,6 @@
     - Modified version of [LedControl](http://wayoda.github.io/LedControl/) by Eberhard Fahle <e.fahle@wayoda.org> v1.0.6. The files are included in this repository.
 
 #### Setup
+
 Simply clone this repository inside `~/Arduino` folder. Open `leddy/leddy.ino` inside the Arduino IDE and select your arduino board at the top of your editor.
+
--- servy/main.py
+++ servy/main.py
@@ -1,35 +1,80 @@
 import machine
-pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
-
-html = """<!DOCTYPE html>
-<html>
-    <head> <title>ESP8266 Pins</title> </head>
-    <body> <h1>ESP8266 Pins</h1>
-        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
-    </body>
-</html>
-"""
 
 import socket
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
 s = socket.socket()
 s.bind(addr)
-s.listen(1)
+s.listen(4)
+print('[MAIN] Listening on', addr)
 
-print('listening on', addr)
+USAGE = """
+
+---
+Usage: HTTP Post with the following plaintext body:
+
+COMMAND [command opts ...]
+
+Commands:
+    ScrollingText <text>
+    ClearDisplay
+---
+"""
+
+def process_body(body):
+    command = body.split(' ')[0]
+    if command == "ScrollingText":
+        text = body[len(command)+1:] # +1 for space
+        if text:
+            # TODO
+            return f"Displaying scrolling text \"{text}\""
+        raise Exception("No text to display")
+    elif command == "ClearDisplay":
+        return f"Cleared display"
+
+    raise Exception(f"No such command: \"{command}\"")
+
 
 while True:
-    cl, addr = s.accept()
-    print('client connected from', addr)
-    cl_file = cl.makefile('rwb', 0)
-    while True:
-        line = cl_file.readline()
-        if not line or line == b'\r\n':
-            break
-    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
-    response = html % '\n'.join(rows)
-    cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-    cl.send(response)
-    cl.close()
+    conn, addr = s.accept()
+    print('\n[MAIN] client connected from ', addr)
+
+    try:
+        line = conn.readline()
+        print(f'[MAIN] Request: {line}')
+        if line[:6] == b'POST /':
+            # Read headers
+            while line and line != b'\r\n':
+                line = conn.readline()
+
+            # Read body
+            try:
+                conn.settimeout(0.1)
+                body = conn.readline()
+            except:
+                raise Exception('No body')
+            if body:
+                body = body.decode('UTF-8').strip()
+            if not body: # Could be empty after strip
+                raise Exception(f"Empty body")
+            print(f'[MAIN] Body: {body}')
+
+            # Process body
+            reply = process_body(body)
+
+            # Reply
+            conn.send('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n')
+            conn.send(reply)
+        else:
+            # Read headers
+            while line and line != b'\r\n':
+                line = conn.readline()
+
+            raise Exception("Only POST is not implemented")
+    except Exception as e:
+        print(f"[MAIN] Error occured: {e}")
+        conn.send('HTTP/1.0 400 Bad request :(\r\nContent-type: text/plain\r\n\r\n')
+        conn.send(f"Error: {e}{USAGE}")
+
+    conn.close()
 
